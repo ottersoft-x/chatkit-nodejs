9682767254fffea5fd5fc84e26b50ff176332c56
Armando Garcia <armando.garcia@ottersoft.com>
2026-06-19 20:49:08 -0700

    Add open stream error primitives

--- src/errors.ts
+++ src/errors.ts
@@ -17,3 +17,38 @@ export class ValidationError extends ChatKitError {
     super(message);
   }
 }
+
+export type StreamErrorCode = "stream.error" | "custom" | (string & {});
+
+export interface StreamErrorOptions {
+  allowRetry?: boolean;
+}
+
+function defaultAllowRetry(code: StreamErrorCode): boolean {
+  return code === "stream.error";
+}
+
+export class StreamError extends ChatKitError {
+  readonly allowRetry: boolean;
+
+  constructor(
+    readonly code: StreamErrorCode,
+    options: StreamErrorOptions = {},
+  ) {
+    super(code);
+    this.allowRetry = options.allowRetry ?? defaultAllowRetry(code);
+  }
+}
+
+export class CustomStreamError extends ChatKitError {
+  readonly code = "custom";
+  readonly allowRetry: boolean;
+
+  constructor(
+    message: string,
+    options: StreamErrorOptions = {},
+  ) {
+    super(message);
+    this.allowRetry = options.allowRetry ?? false;
+  }
+}
